c8ee08d0c204f3ffdd315a956de01f85cd7f4f69
root <root@megvel.com>
2014-11-25 11:31:24 +0300

    Working ubuntu build
    Signed-off-by: root <root@megvel.com>

--- packaging/windows-x-compile-linux.sh
+++ packaging/windows-x-compile-linux.sh
@@ -21,11 +21,11 @@ mkdir $OUT_PATH/printsupport
 
 #compile the app
 cd $OUT_PATH
-MINGW=/opt/mxe/usr
+MINGW=/opt/mingw
 export PATH=$PATH:$MINGW/bin
 export CFLAGS=""
 export CXXFLAGS="$CFLAGS"
-cmake $CODE_PATH -DCMAKE_TOOLCHAIN_FILE=$CODE_PATH/packaging/cmake-modules/Win32Tool1chainchain.cmake -DCMAKE_MODULE_PATH=$CODE_PATH/packaging/cmake-modules
+cmake $CODE_PATH -DCMAKE_TOOLCHAIN_FILE=$CODE_PATH/packaging/cmake-modules/Win32Toolchain.cmake -DCMAKE_MODULE_PATH=$CODE_PATH/packaging/cmake-modules
 
 #cmake $CODE_PATH -DCMAKE_TOOLCHAIN_FILE=/opt/mxe/usr/i686-pc-mingw32/share/cmake/mxe-conf.cmake
 make
